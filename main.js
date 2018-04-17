a4f7a3fef2ac547704c938a4e327f835631d8d8c
Jordan Center <BigJman1234567@yahoo.com>
2018-04-17 11:06:54 -0500

    finished component communication example #2: event dispatcher

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
+window.Event = new class {
+    constructor() {
+        this.vue = new Vue();
+    }
+
+    fire(event, data = null) {
+        this.vue.$emit(event, data);
+    }
+
+    listen(event, callback) {
+        this.vue.$on(event, callback);
+    }
+};
+
 Vue.component('coupon', {
     template: '<input placeholder="Enter your coupon code" @blur="onCouponApplied">',
 
     methods: {
         onCouponApplied() {
-            this.$emit('applied');
+            Event.fire('applied');
+
+
         }
     }
 });
@@ -19,5 +35,9 @@ new Vue({
         onCouponApplied() {
             this.couponApplied = true;
         }
+    },
+
+    created() {
+        Event.listen('applied', () => alert('handling it!'));
     }
 });
